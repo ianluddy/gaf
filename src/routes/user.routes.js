1ec43dd6e7f18bba37b3e7eb2b764cccee78bada
ian luddy <ianluddy@gmail.com>
2018-05-28 21:14:18 +0100

    Babel

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,58 +1,58 @@
-const Router = require("koa-router");
+const Router = require('koa-router');
 const userService = require('../services/user.service.js');
-const ObjectID = require("mongodb").ObjectID;
+const { ObjectID } = require('mongodb').ObjectID;
 const HttpStatus = require('http-status-codes');
-const auth = require("../utils/auth.js");
+const auth = require('../utils/auth.js');
 
 const router = new Router();
 
-router.post("/login", async (ctx) => {
-  const email = ctx.request.body.email;
-  const password = ctx.request.body.password;
+router.post('/login', async (ctx) => {
+  const { email } = ctx.request.body;
+  const { password } = ctx.request.body;
   const user = await userService.validatePassword(email, password);
   if (user) {
-    ctx.body = { token: auth.issue({ email: user.email, id: user._id }) }
+    ctx.body = { token: auth.issue({ email: user.email, id: user._id }) };
   }
 });
 
-router.post("/register", auth.verificationHandler(), async (ctx) => {
+router.post('/register', auth.verificationHandler(), async (ctx) => {
   await userService.insert(ctx.request.body);
   ctx.status = HttpStatus.CREATED;
 });
 
 router.use(auth.errorHandler()).use(auth.jwt());
 
-router.get("/user/:id", auth.verificationHandler(), async (ctx) => {
-  if (ctx.params.id != ctx.state.user._id)
+router.get('/user/:id', auth.verificationHandler(), async (ctx) => {
+  if (ctx.params.id !== ctx.state.user._id.toString()) {
     ctx.throw(HttpStatus.UNAUTHORIZED);
+  }
 
-  const query = {"_id": ObjectID(ctx.params.id)};
-  const user = ctx.state.user;
+  const query = { _id: ObjectID(ctx.params.id) };
 
-  ctx.body = await userService.findOne(query, user);
+  ctx.body = await userService.findOne(query, ctx.state.user);
   ctx.status = HttpStatus.OK;
 });
 
-router.put("/user/:id", auth.verificationHandler(), async (ctx) => {
-  if (ctx.params.id != ctx.state.user._id)
+router.put('/user/:id', auth.verificationHandler(), async (ctx) => {
+  if (ctx.params.id !== ctx.state.user._id.toString()) {
     ctx.throw(HttpStatus.UNAUTHORIZED);
+  }
 
-  const query = {"_id": ObjectID(ctx.params.id)};
+  const query = { _id: ObjectID(ctx.params.id) };
   const update = ctx.request.body;
-  const user = ctx.state.user;
 
-  ctx.body = await userService.update(query, update, user);
+  ctx.body = await userService.update(query, update, ctx.state.user);
   ctx.status = HttpStatus.OK;
 });
 
-router.delete("/user/:id", auth.verificationHandler(), async (ctx) => {
-  if (ctx.params.id != ctx.state.user._id)
+router.delete('/user/:id', auth.verificationHandler(), async (ctx) => {
+  if (ctx.params.id !== ctx.state.user._id.toString()) {
     ctx.throw(HttpStatus.UNAUTHORIZED);
+  }
 
-  const query = {"_id": ObjectID(ctx.params.id)};
-  const user = ctx.state.user;
+  const query = { _id: ObjectID(ctx.params.id) };
 
-  ctx.body = await userService.remove(query, user);
+  ctx.body = await userService.remove(query, ctx.state.user);
   ctx.status = HttpStatus.OK;
 });
 
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,14 @@
-const cfg = require('../config.js');
-const errors = require('../utils/errors.js');
-const bcrypt = require("bcrypt");
-const userCollection = require('monk')(cfg.MONGO_URL).get('user');
+import { ERRORS, ApiError } from '../utils/errors';
+import { MONGO_URL, SALT_ROUNDS } from '../config';
+
+const bcrypt = require('bcrypt');
+const userCollection = require('monk')(MONGO_URL).get('user');
+
+async function validateEmail(email) {
+  if (await userCollection.findOne({ email })) {
+    throw new ApiError(ERRORS.USER_EXISTS);
+  }
+}
 
 async function findOne(query) {
   return userCollection.findOne(query);
@@ -14,34 +21,33 @@ async function remove(query) {
 async function update(query, data) {
   const user = await findOne(query);
 
-  if (user.email != data.email)
+  if (user.email !== data.email) {
     await validateEmail(data.email);
+  }
 
-  data.password = bcrypt.hashSync(data.password, cfg.SALT_ROUNDS);
+  data.password = bcrypt.hashSync(data.password, SALT_ROUNDS);
   return userCollection.update(query, data);
 }
 
 async function insert(data) {
   await validateEmail(data.email);
-  data.password = bcrypt.hashSync(data.password, cfg.SALT_ROUNDS);
+
+  data.password = bcrypt.hashSync(data.password, SALT_ROUNDS);
   return userCollection.insert(data);
 }
 
 async function validatePassword(email, password) {
   const user = await findOne({ email });
 
-  if (!user)
-    throw new errors.ApiError(errors.ERRORS.USER_NOT_FOUND);
+  if (!user) {
+    throw new ApiError(ERRORS.USER_NOT_FOUND);
+  }
 
-  if (!bcrypt.compareSync(password, user.password))
-    throw new errors.ApiError(errors.ERRORS.PASSWORD_INCORRECT);
+  if (!bcrypt.compareSync(password, user.password)) {
+    throw new ApiError(ERRORS.PASSWORD_INCORRECT);
+  }
 
   return user;
-}
-
-async function validateEmail(email) {
-  if (await userCollection.findOne({ email }))
-    throw new errors.ApiError(errors.ERRORS.USER_EXISTS);
 }
 
 module.exports.remove = remove;
